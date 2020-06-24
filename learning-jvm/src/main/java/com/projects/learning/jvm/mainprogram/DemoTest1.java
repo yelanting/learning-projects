82d9fcaa4c8a385e097211500610a51dd9179a55
yelanting <sunlpmail@126.com>
2020-06-24 15:15:40 +0800

    提交jvm的学习文档

---
+++ learning-jvm/src/main/java/com/projects/learning/jvm/mainprogram/DemoTest1.java
@@ -0,0 +1,9 @@
+package com.projects.learning.jvm.mainprogram;
+
+public class DemoTest1 {
+	public static void main(String[] args) {
+		final String STR = "Hello " + "World";
+		System.out.println(STR);
+	}
+
+}
